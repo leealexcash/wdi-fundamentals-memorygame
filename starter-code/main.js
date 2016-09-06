e99fb07a0069eeafaa43668f2c8fb9630f9fe704
Lee Cash <leealexandercash@gmail.com>
2016-09-06 14:51:49 +0100

    cards now being generated by javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,27 @@
-console.log("JS file is connected to HTML! Woo!")
+/* console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
+ if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again");
-};
+}; */
+
+var createBoard = function() {
+  
+  	var gameBoard = document.getElementById('game-board');
+
+ for (i=0; i<4; i++) {
+ 	var newCard = document.createElement('div');
+ 	newCard.className = 'card';
+ 	gameBoard.appendChild(newCard);
+   
+ }
+
+ }
+ 
+ createBoard();
